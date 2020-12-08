eedf2a4b61007960f053d430afc4dd8985ca8580
Daniel Stockhausen <git@daniel-stockhausen.de>
2020-12-08 18:58:34 +0100

    Day 07 refactoring done!

--- aoc/day07/day07.py
+++ aoc/day07/day07.py
@@ -6,62 +6,60 @@ def process_input(filename: str) -> List[List[str]]:
         contained_bags_by_bag = {}
         for bag_line in f.read().splitlines():
             bag, contained_bags = bag_line.split(' bags contain ')
-            contained_bags_by_bag[bag] = contained_bags_str_to_dict(contained_bags)
+            contained_bags_by_bag[bag] = contained_bags_list_to_dict(contained_bags.split(', '))
 
         return contained_bags_by_bag
 
 
-def contained_bags_str_to_dict(contained_bags_str: str) -> dict:
-    bag_count_by_color = {}
-    contained_bags = contained_bags_str.split(', ')
-    for bag_color_with_count in contained_bags:
-        if bag_color_with_count == 'no other bags.':
+def contained_bags_list_to_dict(contained_bags: list) -> dict:
+    contained_bag_count_by_bag = {}
+    for bag_with_count in contained_bags:
+        if bag_with_count == 'no other bags.':
             continue
-        bag_color_with_count_split = bag_color_with_count.split()
-        count = bag_color_with_count_split[0]
-        color = ' '.join(bag_color_with_count_split[1:len(bag_color_with_count_split) - 1])
-        bag_count_by_color[color] = count
-    return bag_count_by_color
+        bag_with_count_split = bag_with_count.split()
+        count = int(bag_with_count_split[0])
+        bag = ' '.join(bag_with_count_split[1:-1])
+        contained_bag_count_by_bag[bag] = count
+    return contained_bag_count_by_bag
 
 
-def bag_fits_in_bags_from_file(color: str, filename: str):
-    rules = process_input(filename)
-    return len(bag_fits_in_bags(color, rules))
+def count_possible_container_bags(bag: str, filename: str):
+    contained_bags_by_bag = process_input(filename)
+    return len(bag_fits_in_bags(bag, contained_bags_by_bag))
 
 
-def bag_fits_in_bags(color: str, rules: dict):
-    compatible_colors = set()
+def bag_fits_in_bags(bag_to_fit: str, contained_bags_by_bag: dict):
+    container_bags = set()
 
-    for rule_color, rule in rules.items():
-        if color in rule:
-            compatible_colors.add(rule_color)
+    for bag, contained_bags in contained_bags_by_bag.items():
+        if bag_to_fit in contained_bags:
+            container_bags.add(bag)
 
-    compatible_colors_copy = compatible_colors.copy()
-    for color in compatible_colors_copy:
-        mre_colors = bag_fits_in_bags(color, rules)
-        compatible_colors.update(mre_colors)
+    for bag_to_fit in set(container_bags):
+        parent_container_bags = bag_fits_in_bags(bag_to_fit, contained_bags_by_bag)
+        container_bags.update(parent_container_bags)
 
-    return compatible_colors
-
-
-def count_bags_from_file(color: str, filename: str) -> int:
-    rules = process_input(filename)
-    return count_bags(color, rules)
+    return container_bags
 
 
-def count_bags(color: str, rules: dict) -> int:
-    more_bags = rules[color]
+def bag_count_inside_bag_from_file(bag: str, filename: str) -> int:
+    contained_bags_by_bag = process_input(filename)
+    return bag_count_inside_bag(bag, contained_bags_by_bag)
 
-    count = 0
-    for bag_color, bag_count in more_bags.items():
-        inner_count = count_bags(bag_color, rules)
-        count = count + int(bag_count) + int(bag_count) * inner_count
-    return count
+
+def bag_count_inside_bag(bag: str, contained_bags_by_bag: dict) -> int:
+    child_bags = contained_bags_by_bag[bag]
+
+    total_bags = 0
+    for bag, bag_count in child_bags.items():
+        inside_total_bags = bag_count_inside_bag(bag, contained_bags_by_bag)
+        total_bags = total_bags + bag_count * (inside_total_bags + 1)
+    return total_bags
 
 
 if __name__ == '__main__':
-    part1 = bag_fits_in_bags_from_file('shiny gold', 'input/day07.txt')
+    part1 = count_possible_container_bags('shiny gold', 'input/day07.txt')
     print(f"Part 1: {part1}")
 
-    part2 = count_bags_from_file('shiny gold', 'input/day07.txt')
+    part2 = bag_count_inside_bag_from_file('shiny gold', 'input/day07.txt')
     print(f"Part 2: {part2}")
--- test/test_day07.py
+++ test/test_day07.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from aoc.day07.day07 import count_bags_from_file, bag_fits_in_bags_from_file
+from aoc.day07.day07 import count_possible_container_bags, bag_count_inside_bag_from_file
 
 file_input = 'input/day07.txt'
 file_example = 'input/day07-example.txt'
@@ -10,16 +10,16 @@ file_example2 = 'input/day07-example2.txt'
 
 class TestDay07(unittest.TestCase):
     def test_07a_example(self):
-        self.assertEqual(11, bag_fits_in_bags_from_file('shiny gold', file_example))
+        self.assertEqual(4, count_possible_container_bags('shiny gold', file_example))
 
     def test_07a(self):
-        self.assertEqual(7283, bag_fits_in_bags_from_file('shiny gold', file_input))
+        self.assertEqual(112, count_possible_container_bags('shiny gold', file_input))
 
     def test_07b_example(self):
-        self.assertEqual(126, count_bags_from_file('shiny gold', file_example2))
+        self.assertEqual(126, bag_count_inside_bag_from_file('shiny gold', file_example2))
 
     def test_07b(self):
-        self.assertEqual(6260, count_bags_from_file('shiny gold', file_input))
+        self.assertEqual(6260, bag_count_inside_bag_from_file('shiny gold', file_input))
 
-    # def test_07_main(self):
-    #     self.assertEqual(0, os.system("python -m aoc.day07.day07"))
+    def test_07_main(self):
+        self.assertEqual(0, os.system("python -m aoc.day07.day07"))
